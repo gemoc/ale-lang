14f24e142abd202d1448bb402c4b66003f38b030
Fabien Coulon <fabien.coulon@inria.fr>
2017-03-13 15:38:04 +0100

    [Grammar] Add RuntimeClass definition

---
+++ lang.core/src-gen/implementation/RuntimeClass.java
@@ -0,0 +1,87 @@
+/**
+ */
+package implementation;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Runtime Class</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * </p>
+ * <ul>
+ *   <li>{@link implementation.RuntimeClass#getName <em>Name</em>}</li>
+ *   <li>{@link implementation.RuntimeClass#getAttributes <em>Attributes</em>}</li>
+ *   <li>{@link implementation.RuntimeClass#getMethods <em>Methods</em>}</li>
+ * </ul>
+ *
+ * @see implementation.ImplementationPackage#getRuntimeClass()
+ * @model
+ * @generated
+ */
+public interface RuntimeClass extends EObject {
+	/**
+	 * Returns the value of the '<em><b>Name</b></em>' attribute.
+	 * The default value is <code>""</code>.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Name</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Name</em>' attribute.
+	 * @see #setName(String)
+	 * @see implementation.ImplementationPackage#getRuntimeClass_Name()
+	 * @model default="" required="true"
+	 * @generated
+	 */
+	String getName();
+
+	/**
+	 * Sets the value of the '{@link implementation.RuntimeClass#getName <em>Name</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Name</em>' attribute.
+	 * @see #getName()
+	 * @generated
+	 */
+	void setName(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Attributes</b></em>' containment reference list.
+	 * The list contents are of type {@link implementation.VariableDeclaration}.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Attributes</em>' containment reference list isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Attributes</em>' containment reference list.
+	 * @see implementation.ImplementationPackage#getRuntimeClass_Attributes()
+	 * @model containment="true"
+	 * @generated
+	 */
+	EList<VariableDeclaration> getAttributes();
+
+	/**
+	 * Returns the value of the '<em><b>Methods</b></em>' containment reference list.
+	 * The list contents are of type {@link implementation.Method}.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Methods</em>' containment reference list isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Methods</em>' containment reference list.
+	 * @see implementation.ImplementationPackage#getRuntimeClass_Methods()
+	 * @model containment="true"
+	 * @generated
+	 */
+	EList<Method> getMethods();
+
+} // RuntimeClass
