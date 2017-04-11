b07b713801e8094e0916811bd68d598b7c3457f7
Fabien Coulon <fabien.coulon@inria.fr>
2017-04-11 13:50:59 +0200

    [Eval] Fix eAllContent() for ExtendedClass

--- org.eclipse.emf.ecoretools.ale.core/src/org/eclipse/emf/ecoretools/ale/core/interpreter/services/DynamicEObjectServices.java
+++ org.eclipse.emf.ecoretools.ale.core/src/org/eclipse/emf/ecoretools/ale/core/interpreter/services/DynamicEObjectServices.java
@@ -11,6 +11,7 @@
 package org.eclipse.emf.ecoretools.ale.core.interpreter.services;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,6 +19,9 @@ import org.eclipse.acceleo.query.runtime.CrossReferenceProvider;
 import org.eclipse.acceleo.query.runtime.IReadOnlyQueryEnvironment;
 import org.eclipse.acceleo.query.runtime.IRootEObjectProvider;
 import org.eclipse.acceleo.query.services.EObjectServices;
+import org.eclipse.emf.common.util.AbstractTreeIterator;
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecoretools.ale.core.interpreter.DynamicFeatureRegistry;
 
@@ -57,5 +61,38 @@ public class DynamicEObjectServices extends EObjectServices{
 	@Override
 	public EObject eContainer(EObject eObject) {
 		return dynamicFeatures.getInstanceOrSelf(eObject.eContainer());
+	}
+	
+	@Override
+	public EObject eContainer(EObject eObject, EClass type) {
+		final EObject result;
+
+		EObject current = eContainer(eObject);
+		while (current != null && !type.isSuperTypeOf(current.eClass()) && !type.isInstance(current)) {
+			current = eContainer(eObject);
+		}
+		if (current != null && (type.isSuperTypeOf(current.eClass()) || type.isInstance(current))) {
+			result = current;
+		} else {
+			result = null;
+		}
+
+		return result;
+	}
+	
+	@Override
+	public List<EObject> eAllContents(EObject eObject) {
+		return Lists.newArrayList(dynamicEAllContents(eObject));
+	}
+	
+	private TreeIterator<EObject> dynamicEAllContents(EObject eObject) {
+		return new AbstractTreeIterator<EObject>(eObject, false) {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public Iterator<EObject> getChildren(Object object) {
+				return eContents((EObject) object).iterator();
+			}
+		};
 	}
 }
