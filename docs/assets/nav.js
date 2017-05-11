f066ab32b9077e52531e556bac7a357f299d6a5e
Fabien Coulon <fabien.coulon@inria.fr>
2017-05-11 19:14:58 +0200

    [Web site] Add a navigation menu based on titles

---
+++ docs/assets/nav.js
@@ -0,0 +1,7 @@
+$(function(){
+
+  $("section h1, section h2").each(function(){
+	 $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
+	 $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
+  });
+}); 
