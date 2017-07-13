d747f0ea0660a921e915a59a36ebe21ed70315ea
Fabien Coulon <fabien.coulon@inria.fr>
2017-07-13 14:19:16 +0200

    [IDE] Convert all URI to file path

--- plugins/org.eclipse.emf.ecoretools.ale.ide/src/org/eclipse/emf/ecoretools/ale/ide/WorkbenchDsl.java
+++ plugins/org.eclipse.emf.ecoretools.ale.ide/src/org/eclipse/emf/ecoretools/ale/ide/WorkbenchDsl.java
@@ -11,19 +11,24 @@
 package org.eclipse.emf.ecoretools.ale.ide;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecoretools.ale.core.parser.Dsl;
+import org.osgi.framework.Bundle;
 
 public class WorkbenchDsl extends Dsl {
 
 	public WorkbenchDsl(String dslFile) throws FileNotFoundException {
-		super(dslFile);
+		super(convertToFile(dslFile));
 		resolveUris();
 	}
 	
@@ -34,22 +39,77 @@ public class WorkbenchDsl extends Dsl {
 	
 	private void resolveUris() {
 		ArrayList<String> newSemantics = new ArrayList<String>();
-		IWorkspace ws = ResourcesPlugin.getWorkspace();
 		getAllSemantics()
 			.stream()
-			.forEach(elem -> {
-				URI uri = URI.createURI(elem);
-				if(ws != null && uri.isPlatform()) {
-					IResource file = ws.getRoot().findMember(uri.toPlatformString(true));
-					String path = file.getLocationURI().getRawPath();
-					newSemantics.add(path);
-				}
-				else {
-					newSemantics.add(elem);
-				}
-			});
+			.forEach(elem -> newSemantics.add(URI.createFileURI(convertToFile(elem)).toFileString()));//expect system file path
 		getAllSemantics().clear();
 		getAllSemantics().addAll(newSemantics);
+		
+		ArrayList<String> newSyntaxes = new ArrayList<String>();
+		getAllSyntaxes()
+			.stream()
+			.forEach(elem -> newSyntaxes.add(URI.createFileURI(convertToFile(elem)).toString()));//expect file protocol
+		getAllSyntaxes().clear();
+		getAllSyntaxes().addAll(newSyntaxes);
 	}
 	
+	/**
+	 * Convert platform URI to file path
+	 */
+	private static String convertToFile(String path) {
+		URI uri = URI.createURI(path);
+		
+		String res = null;
+		
+		if(uri.isPlatformResource()) {
+			res = resourceToFile(uri);
+			if(res == null) {
+				res = pluginToFile(resourceToPlugin(uri));
+			}
+		}
+		else if(uri.isPlatformPlugin()) {
+			res = pluginToFile(uri);
+		}
+		
+		if(res == null) {
+			res = path;
+		}
+		
+		return res;
+	}
+	
+	private static String resourceToFile(URI uri) {
+		IWorkspace ws = ResourcesPlugin.getWorkspace();
+		if(ws != null) {
+			IResource file = ws.getRoot().findMember(uri.toPlatformString(true));
+			if(file != null) {
+				return file.getLocationURI().getRawPath();
+			}
+		}
+		return null;
+	}
+	
+	private static String pluginToFile(URI uri) {
+		String pluginName = uri.segment(1);
+		String path = uri.toPlatformString(true).substring(pluginName.length()+1);
+		
+		Bundle plugin = Platform.getBundle(pluginName);
+		URL pluginURL = plugin.getEntry("/");
+		
+		try {
+			String pluginFilePath = FileLocator.toFileURL(pluginURL).getFile();
+			return  pluginFilePath.toString() + path.substring(1);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Convert platform:/resource/ to platform:/plugin/
+	 */
+	private static URI resourceToPlugin(URI uri) {
+		return uri.createPlatformPluginURI(uri.toPlatformString(true), true);
+	}
 }
