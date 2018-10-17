1227a7de1dec322efb93dd5aef83873ffb375f1e
Fabien Coulon <fabien.coulon@obeo.fr>
2018-10-17 16:45:39 +0200

    Clean up Logo example

--- examples/logo.example/src/logo/example/service/Display.java
+++ examples/logo.example/src/logo/example/service/Display.java
@@ -19,7 +19,6 @@ public class Display {
 	public static void show(EObject turtle) {
 		JFrame frame = new JFrame();
 		frame.setBounds(100, 100, 1000, 1000);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Logo example");
 		frame.setVisible(true);
 		frame.add(new Board(turtle));
