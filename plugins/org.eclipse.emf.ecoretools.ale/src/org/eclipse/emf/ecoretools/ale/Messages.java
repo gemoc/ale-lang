14444f7aaa4afb37cfa30381e0672dd9aa82b5f9
Didier Vojtisek <didier.vojtisek@inria.fr>
2019-11-13 09:20:03 +0100

    add ALEDynamicExpressionEvaluator
    This class offers services similar to AQLSiriusInterpreter but without
    the registration to the workspace listener
    If offers the possibility to parse and evaluate string expressions and
    return various kinds of objects
    It relies on the queryEnvironment of an existing ALEInterpreter. (ie.
    this is the responsibility of this ALEInterpreter
    to clean/dispose the query environment when not use anymore)

---
+++ plugins/org.eclipse.emf.ecoretools.ale/src/org/eclipse/emf/ecoretools/ale/Messages.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.emf.ecoretools.ale;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * Helper class to obtains translated strings.
+ * 
+ */
+
+
+public final class Messages  extends NLS {
+	private static final String BASE_NAME = "org.eclipse.emf.ecoretools.ale.messages"; //$NON-NLS-1$
+
+    static {
+    	// load message values from bundle file
+    	NLS.initializeMessages(BASE_NAME, Messages.class);
+    }
+
+    public static String ALEInterpreter_errorWithExpression;
+
+   
+}
