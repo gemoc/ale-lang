f2c4d8a43ed46cf23bf14cb1c28a1160fe62d4ce
Emmanuel Chebbi <emmanuel.chebbi@outlook.fr>
2020-05-04 17:30:06 +0200

    [Console] Provide shorter error messages in a red font (#124)
    Because:
     - current error messages are bloated with duplicate info
       and internal, irrelevant stacktraces
    
    Signed-off-by: Emmanuel Chebbi <emmanuel.chebbi@outlook.fr>

--- plugins/org.eclipse.emf.ecoretools.ale.core/src/org/eclipse/emf/ecoretools/ale/core/interpreter/DiagnosticLogger.java
+++ plugins/org.eclipse.emf.ecoretools.ale.core/src/org/eclipse/emf/ecoretools/ale/core/interpreter/DiagnosticLogger.java
@@ -11,12 +11,17 @@
 package org.eclipse.emf.ecoretools.ale.core.interpreter;
 
 import static java.util.Objects.requireNonNull;
+import static java.util.regex.Pattern.MULTILINE;
 
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import org.eclipse.acceleo.query.ast.Expression;
 import org.eclipse.emf.common.util.BasicDiagnostic;
@@ -26,6 +31,12 @@ import org.eclipse.emf.ecoretools.ale.core.parser.visitor.ParseResult;
 import org.eclipse.emf.ecoretools.ale.implementation.ModelUnit;
 
 public class DiagnosticLogger {
+	
+	/**
+	 * Matches AQL's error messages and returned the actual, wrapped error.
+	 */
+	private static final Pattern AQL_SERVICE_ERROR_PATTERN = Pattern.compile(".*with arguments \\[.*\\].* failed:(.*)", Pattern.DOTALL);
+	
 	List<Diagnostic> log = new ArrayList<>();
 	private final DslSemantics semantics;
 	
@@ -54,10 +65,10 @@ public class DiagnosticLogger {
 	public void diagnosticForHuman( BasicDiagnostic diagnostic) {
 		diagnostic
 			.getChildren()
-			.stream()
 			.forEach(
 				diag -> {
-					if(diag.getSource().equals(MethodEvaluator.PLUGIN_ID)){
+					// We only want to display our own errors
+					if (diag.getSource().equals(MethodEvaluator.PLUGIN_ID)) {
 						Expression failedExp = (Expression) diag.getData().get(0);
 						Diagnostic diagExp = diag;
 						
@@ -65,30 +76,41 @@ public class DiagnosticLogger {
 						if(diag.getData().size() > 1) {
 							diagExp = (Diagnostic) diag.getData().get(1);
 						}
-						printError(failedExp, diagExp.toString());
+						printError(failedExp, diagExp);
 						if(diagExp instanceof BasicDiagnostic) {
 							diagnosticForHuman((BasicDiagnostic) diagExp);
 						}
-					} else {
-						if(!diag.getData().isEmpty() && diag.getData().get(0) instanceof Exception) {
-							Exception e = (Exception) diag.getData().get(0);
-							if(e.getCause() instanceof CriticalFailure) {
-								CriticalFailure interpreterFailure = (CriticalFailure) e.getCause();
-								diagnosticForHuman(interpreterFailure.diagnostics);
-							} else if(e.getCause() != null){
-								e.getCause().printStackTrace();
-							}
-						}
 					}
+					
+					// TODO Consider removing the code below
+					//		I comment it because it exposes internal and irrelevant errors
+					//		to the user. However, it may be useful for debugging purposes
+					//		and I prefer to comment it out: it will be easier to find it.
+					
+//					else {
+//						if(!diag.getData().isEmpty() && diag.getData().get(0) instanceof Exception) {
+//							Exception e = (Exception) diag.getData().get(0);
+//							if(e.getCause() instanceof CriticalFailure) {
+//								CriticalFailure interpreterFailure = (CriticalFailure) e.getCause();
+//								diagnosticForHuman(interpreterFailure.diagnostics);
+//							} else if(e.getCause() != null){
+//								e.getCause().printStackTrace();
+//							}
+//						}
+//					}
 				}
 			);
 	}
 	
-	private void printError(Expression expr, String errorMsg) {
+	private void printError(Expression expr, Diagnostic diagnostic) {
 		ParseResult<ModelUnit> parsedFile = semantics.findParsedFileDefining(expr).orElse(null);
 		if (parsedFile == null) {
-			System.err.println("\n[AQL eval fail] At unknown file and line (" + expr + ")");
-			System.err.println(errorMsg);
+			System.err.println("\nAt unknown file and line (" + expr + "):\n");
+			Stream.concat(Stream.of(diagnostic), diagnostic.getChildren().stream())
+				  .map(Diagnostic::getMessage)
+				  .map(getActualError())
+				  .filter(msg -> msg != null && !msg.trim().isEmpty())
+				  .forEach(message -> System.err.println("    " + message));
 		}
 		else {
 			// TODO [Refactor] Optional<Integer> start = parsedFile.getStartPositionOf(expr)
@@ -96,9 +118,33 @@ public class DiagnosticLogger {
 			if(startPos != null) {
 				String file = parsedFile.getSourceFile();
 				int line =  getLine(startPos,file);
-				System.err.println("\n[AQL eval fail] At line "+ line +" in " + file + ":\n" + errorMsg);
+				System.err.println("\nAt line "+ line +" in " + file + ":");
+				Stream.concat(Stream.of(diagnostic), diagnostic.getChildren().stream())
+					  .map(Diagnostic::getMessage)
+					  .filter(msg -> msg != null && !msg.trim().isEmpty())
+					  .map(getActualError())
+					  .forEach(message -> System.err.println("    " + message));
 			}
 		}
+	}
+	
+	/**
+	 * Returns the wrapped error message or the message itself if it's not wrapped.
+	 * <p>
+	 * Required because AQL catch exceptions thrown by services and directly modifies
+	 * their (so, our) error message. 
+	 */
+	private Function<String, String> getActualError() {
+		return message -> {
+			Matcher matcher = AQL_SERVICE_ERROR_PATTERN.matcher(message);
+			
+			if (matcher.matches()) {
+				return matcher.group(1).trim();
+			}
+			else {
+				return message;
+			}
+		};
 	}
 	
 	private int getLine(int offset, String file) {
